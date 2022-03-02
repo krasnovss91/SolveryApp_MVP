25ad23e51549154a8e582956ae7f8d3285ed0ecf
krasnovss91 <krasnovstanislav91@gmail.com>
2022-03-02 12:49:18 +0400

    DTO

--- app/src/main/java/com/example/solveryapp_mvp/Product.kt
+++ app/src/main/java/com/example/solveryapp_mvp/Product.kt
@@ -3,6 +3,25 @@ package com.example.solveryapp_mvp
 import android.os.Parcelable
 import androidx.annotation.DrawableRes
 import kotlinx.android.parcel.Parcelize
+import java.time.OffsetDateTime
 
 @Parcelize
-data class Product(@DrawableRes val avatar: Int, val name: String, val producer: String, val cost: Int, val id: Int):Parcelable
+data class Product(
+    @DrawableRes val avatar: Int,
+    val name: String,
+    val producer: String,
+    val cost: Int,
+    val id: Int,
+    val offsetDateTime: OffsetDateTime
+    ):Parcelable
+
+@Parcelize
+data class ProductDto(
+    @DrawableRes val avatar: Int,
+    val name: String,
+    val producer: String,
+    val cost: Int,
+    val id: Int,
+    val offsetDateTime: OffsetDateTime,
+    val metadata: Metadata
+):Parcelable
