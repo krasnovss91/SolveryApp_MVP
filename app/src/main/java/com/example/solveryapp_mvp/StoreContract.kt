ec3e907b85ee194fa9690b647919b2439474bcbe
krasnovss91 <krasnovstanislav91@gmail.com>
2022-03-02 15:44:52 +0400

    StoreActivity_3

--- app/src/main/java/com/example/solveryapp_mvp/StoreContract.kt
+++ app/src/main/java/com/example/solveryapp_mvp/StoreContract.kt
@@ -2,4 +2,19 @@ package com.example.solveryapp_mvp
 
 interface StoreContract {
 
+    fun showProgress()
+
+    fun hideProgress()
+
+    fun showError()
+
+    fun hideError()
+
+    fun hideContent()
+
+    fun showContent(content: List<ProductViewState>)
+
+    fun showContent(show: Boolean)
+
+    fun showError(show: Boolean)
 }
